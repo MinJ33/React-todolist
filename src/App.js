22b6968f0f84efdf3b62eda60145b7d9efb9a228
MinJ33 <erica990604@gmail.com>
2022-09-11 21:53:05 +0900

    feat: Add hard code to see what it's gonna look like

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+// import React, {useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      1. Header
+      {/* 1. Header */}
+    <h1>Todo List App</h1>
+      {/* 2. Input (input and button) */}
 
-      2. Input (input and button)
+    <input
+      type="text"
+      placeholder="Add an item.."
+    />
 
-      3. List of Items (unordered list with items)
+    <button> Add </button>
+
+    {/* 3. List of Items (unordered list with items) */}
+    <ul>
+      <li>Take out the trash</li>
+      <li>Water the plants</li>
+      <li>Go to the gym</li>
+    </ul>
     </div>
   );
 }
